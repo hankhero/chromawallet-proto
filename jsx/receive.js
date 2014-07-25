20849c37e7f067862d382cd56d74e8ff7e9dd4e9
Henrik Hjelte <henrik@henrikhjelte.com>
2014-07-26 00:48:45 +0200

    nicer receive tab

--- jsx/receive.js
+++ jsx/receive.js
@@ -42,13 +42,12 @@ var Receive = React.createClass({
 	  <tbody>
 		<tr>
 		  <td>Bitcoin</td>
-		  <td>123jasa994ncoee9q4n&nbsp;<div className="small primary btn"><a href="#">Copy</a></div></td>
+		  <td className="receive__address-cell"><span className="receive__address">123jasa994ncoee9q4n&nbsp;</span><div className="small primary btn"><a href="#">Copy</a></div></td>
 		  <td>10.23</td>
 		</tr>
 		<tr>
 		  <td>Foo-coin</td>
-		  <td>CoPw4ahijciS1C@mp78983PxHDLX4BdQTSHu4nhHC7W6CEssn&nbsp;<div className="small primary btn"><a href="#">Copy</a></div></td>
-
+		  <td className="receive__address-cell"><span className="receive__address">CoPw4ahijciS1C@mp78983PxHDLX4BdQTSHu4nhHC7W6CEssn&nbsp;</span><div className="small primary btn"><a href="#">Copy</a></div></td>
 		  <td>1.02</td>
 		</tr>
 	  </tbody>
