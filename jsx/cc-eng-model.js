3deaf6ac43dfa0f68d067acff58fb2aab05faaf3
Henrik Hjelte <henrik@henrikhjelte.com>
2014-08-19 14:37:11 +0200

    cc-eng-model does not crash due to history page

--- jsx/cc-eng-model.js
+++ jsx/cc-eng-model.js
@@ -101,37 +101,23 @@ AssetModels.prototype.updateAssetModels = function() {
   })
 }
 
-AssetModels.prototype.getIsLoggedIn = function () {
-    return this.isLoggedIn;
+AssetModels.prototype.isInitialized = function () {
+    return this.isInitializedFlag;
 };
 
 
-AssetModels.prototype.loginClicked = function (loginComponent) {
-    var passphrase = loginComponent.getPassPhrase();
-    if (passphrase === 'test') {
-        this.isLoggedIn = true;
-        loginComponent.forceUpdate(); //We need to trigger a re-render
-    } else {
-        loginComponent.setErrorMessage('Demo error message. The passphrase is test.');
-    }
+AssetModels.prototype.initializeFromSeed = function (seed) {
+    this.isInitializedFlag = true;
+    this.updateCallback();
 };
 
-AssetModels.prototype.createWalletClicked = function (loginComponent) {
-    var passphrase = loginComponent.getPassPhrase();
-    if (window.confirm(
-        'Are you sure you want to create a wallet')) {
-        var masterKey = passphrase; //TODO do sha256(passphrase); (or should we do it inside ccWallet?
-        cc_wallet = new ccWallet({
-            "masterKey": masterKey, 
-            testnet: true});
-        wallet = new AssetModels(cc_wallet);
-        wallet.updateAssetModels();
+AssetModels.prototype.generateRandomSeed = function (entropy) {
+     return 'random seed';
+};
 
-        this.isLoggedIn = true;
-        loginComponent.forceUpdate();
-     } else {
-        loginComponent.setErrorMessage('Try again.');
-     }
+AssetModels.prototype.getHistory = function () {
+    //TODO
+    return [];
 };
 
 
