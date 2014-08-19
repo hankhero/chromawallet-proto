c3b1963e27c8d9e6aa20b481f7e11203d42802a5
Henrik Hjelte <henrik@henrikhjelte.com>
2014-08-19 14:20:57 +0200

    send tab markup fix for label for

--- jsx/send.js
+++ jsx/send.js
@@ -78,11 +78,11 @@ var Send = React.createClass({
 
         <div className="row">
           <div className="two columns">
-            <label className="inline" for="text1">Address:</label>
+            <label className="inline" for="send-address">Address:</label>
           </div>
           <div className="ten columns">          
             <li className="field">
-              <input className="text input" type="text" placeholder="Address" 
+              <input className="text input" id="send-address" type="text" placeholder="Address" 
                      onChange={this.onChangeAddress} value={this.state.address}
               />
             </li>
@@ -90,11 +90,11 @@ var Send = React.createClass({
 
           <div className="row">
             <div className="two columns">
-              <label className="inline" for="text2">Amount:</label>
+              <label className="inline" for="send-amount">Amount:</label>
             </div>
             <div className="four columns">
               <li className="field">
-                <input className="narrow text input" type="text" placeholder="Ammount" 
+                <input className="narrow text input" id="send-amount" type="text" placeholder="Ammount" 
                        onChange={this.onChangeAmmount} value={this.state.amount}
                 />
               </li>
