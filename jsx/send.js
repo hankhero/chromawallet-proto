e3fe0652160e4920359adf71d8dc9ddd39b94877
Henrik Hjelte <henrik@henrikhjelte.com>
2014-08-20 22:02:40 +0200

    span inside option made react.js freak out, fixed.

--- jsx/send.js
+++ jsx/send.js
@@ -1,12 +1,14 @@
 /** @jsx React.DOM */
 
-var AssetLabel = React.createClass({
+var AssetOption = React.createClass({
   render: function () {
     var asset = this.props.asset;
     var available = asset.getAvailableBalance();
     var text = asset.getMoniker() + " (" + available + " available)";
     return (
-      <span>{text}</span>
+      <option value={asset.getMoniker()}>
+            {text}
+     </option>
     );
   }
 });
@@ -142,9 +144,7 @@ var Send = React.createClass({
                       {
                         assets.map(function (asset) {
                           return (
-                            <option value={asset.getMoniker()}>
-                              <AssetLabel asset={asset} />
-                            </option>
+                              <AssetOption asset={asset} />
                           )
                         })
                       }
