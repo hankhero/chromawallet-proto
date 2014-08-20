65072c6e08775e7b2ee361e06d2d7b48eada34b3
Alex Mizrahi <alex.mizrahi@gmail.com>
2014-08-20 19:46:48 +0300

    auto add test asset

--- jsx/cc-eng-model.js
+++ jsx/cc-eng-model.js
@@ -4,6 +4,15 @@ var ccWalletEngine = require('cc-wallet-engine');
 var wallet = new ccWalletEngine();
 wallet.initializeFromSeed('test');
 
+var adm = wallet.wallet.adManager;
+if (adm.getByMoniker('gold') == null) {
+    wallet.wallet.addAssetDefinition({
+            monikers: ['gold'],
+    colorSchemes: ['epobc:b95323a763fa507110a89ab857af8e949810cf1e67e91104cd64222a04ccd0bb:0:180679']
+        });
+    wallet.update();
+}
+
 module.exports = {
     wallet: wallet
 };
