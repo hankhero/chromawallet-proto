99c369fedb614613c7c052236336a599e4062b89
Alex Mizrahi <alex.mizrahi@gmail.com>
2014-09-21 01:23:27 +0300

    cc-eng-model.js debugging stuff

--- jsx/cc-eng-model.js
+++ jsx/cc-eng-model.js
@@ -1,17 +1,31 @@
 /** @jsx React.DOM */
-var ccWalletEngine = require('cc-wallet-engine');
 
-var wallet = new ccWalletEngine();
-wallet.initializeFromSeed('test');
+console.log('loading cc-wallet-engine');
 
-var adm = wallet.wallet.adManager;
-if (adm.getByMoniker('gold') == null) {
-    wallet.wallet.addAssetDefinition({
-            monikers: ['gold'],
-    colorSchemes: ['epobc:b95323a763fa507110a89ab857af8e949810cf1e67e91104cd64222a04ccd0bb:0:180679']
-        });
-    wallet.update();
+var ccWalletEngine = null;
+
+try {
+    ccWalletEngine = require('cc-wallet-engine');
+} catch (x) {
+    console.log('error:' + x.toString());
 }
+
+console.log('done. creating wallet');
+
+var wallet = new ccWalletEngine({testnet: true});
+
+console.log('done!');
+
+//var adm = wallet.wallet.adManager;
+//if (adm.getByMoniker('gold') == null) {
+//    wallet.wallet.addAssetDefinition({
+//            monikers: ['gold'],
+//    colorSchemes: ['epobc:b95323a763fa507110a89ab857af8e949810cf1e67e91104cd64222a04ccd0bb:0:180679']
+//        });
+//    wallet.update();
+//}
+
+//setInterval(function () { wallet.update(); }, 10000);
 
 module.exports = {
     wallet: wallet
