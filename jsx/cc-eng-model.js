fe97140c874dfb1b32596ec90babdc3fc5682d08
Alex Mizrahi <alex.mizrahi@gmail.com>
2014-09-22 05:50:19 +0300

    gold asset definition loaded by default

--- jsx/cc-eng-model.js
+++ jsx/cc-eng-model.js
@@ -12,18 +12,16 @@ try {
 
 console.log('done. creating wallet');
 
-var wallet = new ccWalletEngine({testnet: true});
+var systemAssetDefinitions = [{
+    monikers: ['gold'],
+    colorSchemes: ['epobc:b95323a763fa507110a89ab857af8e949810cf1e67e91104cd64222a04ccd0bb:0:180679']
+    }];
+
+var wallet = new ccWalletEngine(
+    {testnet: true, systemAssetDefinitions: systemAssetDefinitions}
+);
 
 console.log('done!');
-
-//var adm = wallet.wallet.adManager;
-//if (adm.getByMoniker('gold') == null) {
-//    wallet.wallet.addAssetDefinition({
-//            monikers: ['gold'],
-//    colorSchemes: ['epobc:b95323a763fa507110a89ab857af8e949810cf1e67e91104cd64222a04ccd0bb:0:180679']
-//        });
-//    wallet.update();
-//}
 
 wallet.update();
 
