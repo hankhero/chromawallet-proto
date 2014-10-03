2a83a36dd3be15eedd058123556de057f7292ff4
Henrik Hjelte <henrik@henrikhjelte.com>
2014-10-03 17:24:11 +0200

    copied design to react classes in login

--- jsx/login.js
+++ jsx/login.js
@@ -1,6 +1,138 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var React = require('react/addons'); //With addons
+
+var Panel = {
+    getDefaultProps: function () {
+        return {
+            active: false
+        };
+    },
+    nextClick: function () {
+        this.props.parent.nextTab();
+    },
+    render: function () {
+        var active = this.props.active ? ' active': '',
+        classes = "tab-content" + active;
+        return (
+         <div className={classes}>{
+             this.renderContent()
+         }
+
+         </div>
+	     );
+    },
+    renderNextButton: function () {
+        return (
+	        <div className="row">
+	          <div className="push_eight two columns">
+	            <div className="medium secondary btn">
+	              <a href="#" className="switch" onClick={this.nextClick.bind(this)}>Next</a>
+	            </div>
+	          </div>
+	        </div>
+        );
+    }
+};
+
+var WelcomePanel = React.createClass({
+    mixins: [Panel],
+    renderContent: function () {
+        return (
+          <div>
+	        <div className="row">
+	          <div className="ten columns centered text-center">
+	            <h2>Welcome</h2>
+	            <p>To a combined wallet for bitcoins and colored-coins such as gold, art and well basically anything.</p>
+	            <h3>Setup and security</h3>
+	            <p>We will now take you through some steps to secure your wallet. This is necessary for your own security. It does not take long.</p>
+	            <p><em>It is very important!</em></p>
+	            <p>(If you instead need to <a href="#" className="switch active" gumby-trigger="#restore-dialogue | #passphrase-dialogue">restore a wallet, click here</a>)</p>
+	          </div>
+	        </div>
+            {
+                this.renderNextButton()
+            }
+          </div>
+	     );
+    }
+});
+
+var MnemonicPanel = React.createClass({
+    mixins: [Panel],
+    renderContent: function () {
+        return (
+          <div>
+            <div className="row">
+              <div className="ten columns centered text-center">
+                <h2>Mnemonic</h2>
+                <p>We have created some random words that represent the secret key to you wallet. It is very important you<em>write it down</em> and store it in a safe and secret place.</p>
+<p>Please store this phrase somewhere safe and secret.</p>
+                <p className="warning alert">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.&nbsp;&nbsp;Etiam laoreet quam sed arcu.&nbsp;&nbsp;Pellentesque tristique imperdiet tortor.&nbsp;&nbsp;Nam vestibulum accumsan nisl.&nbsp;&nbsp;</p>
+              </div>
+            </div>
+            {
+                this.renderNextButton()
+            }
+          </div>
+	     );
+    }
+});
+
+var PasswordPanel = React.createClass({
+    mixins: [Panel],
+    renderContent: function () {
+        return (
+          <div>
+            <div className="row">
+              <div className="ten columns centered text-center">
+                 <h2>Password</h2>
+                 <p>The password is used every time you open your wallet. It should be difficult but possible to remember.</p>
+                 <p>Please provide a password for daily use. Do not reuse an existing password.</p>
+                 <form>
+                   <div className="field">
+                     <input className="input" placeholder="At least eight letters, not too easy." type="password" />
+                   </div>
+                 </form>
+                 <p className="warning alert">I am an error message</p>
+              </div>
+            </div>
+            {
+                this.renderNextButton()
+            }
+          </div>
+        );
+    }
+});
+
+var PinPanel = React.createClass({
+    mixins: [Panel],
+    renderContent: function () {
+        return (
+          <div>
+
+            <div className="row">
+              <div className="ten columns centered text-center">
+                 <h2>PIN</h2>
+                 <p>The PIN is a number sequence you use every time you send an asset.</p>
+                 <p>Please provide a PIN-code for daily use.</p>
+                 <form>
+                   <div className="field">
+                     <input className="input" placeholder="At least four digits." type="password" />
+                   </div>
+                 </form>
+                 <p className="warning alert">I am an error message</p>
+              </div>
+            </div>
+
+            {
+                this.renderNextButton()
+            }
+          </div>
+        );
+    }
+});
+
 
 var Login = React.createClass({
     setErrorMessage: function (text) {
@@ -11,6 +143,7 @@ var Login = React.createClass({
     },
     getInitialState: function () {
         return {
+            activeTab: 'welcome',
             initializing: false,
             passphrase: '',
             errorMessage: null
@@ -49,56 +182,176 @@ var Login = React.createClass({
             errorMessage: null
         });
     },
+    tabNames: ['welcome','mnemonic', 'password','pin'],
+    nextTab: function () {
+        var i = this.tabNames.indexOf(this.state.activeTab);
+        i = i + 1;
+        if (i >= this.tabNames.length) {
+            i = 0;
+        }
+        this.setState({activeTab: this.tabNames[i]});
+    },
     render: function () {
+        console.log('New login');
         if (this.state.initializing) {
-      return (
-<div className="modal active" id="login-dialogue">
-  <div className="content">
-    <div className="row">
-      <div className="ten columns centered text-center">
-        <h2>Initializing Wallet ...</h2>
-      </div>
-    </div>
-  </div>
-</div>
-      );
-        } else if (this.props.wallet.isInitialized()) {
-            return <div/>;
+            return (
+                    <div className="modal active" id="login-dialogue">
+                    <div className="content">
+                    <div className="row">
+                    <div className="ten columns centered text-center">
+                    <h2>Initializing Wallet ...</h2>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+            );
+// TEMP REMOVED
+//        } else if (this.props.wallet.isInitialized()) {
+//            return <div/>;
         } else {
             var passphrase = this.getPassPhrase(),
-            errorMessage = this.state.errorMessage,
-            warningClasses = errorMessage ? 'warning alert': '';
-      return (
+                errorMessage = this.state.errorMessage,
+                warningClasses = errorMessage ? 'warning alert': '',
+                tabNames = this.tabNames,
+                activeTab = this.state.activeTab,
+                self = this;
+            return (
+                <div className="modal active" id="passphrase-dialogue">
+                  <div className="content">
+                    <section className="pill tabs">
+                      <ul className="tab-nav">
+                      {
+                          tabNames.map(function (tab, i) {
+                              var cx = React.addons.classSet,
+                              c = {
+                                  active: tab === activeTab
+                              },
+                              changeTab = function () {
+                                  self.setState({activeTab:tab});
+                              }
+                              c['tab-' + tab] = true;
+                              return (
+                                  <li className={cx(c)}><a href="#"
+                                      onClick={changeTab}
+                                  >{i}</a></li>
+                              );
+                          })
+                      }
+                      </ul>
+                      <WelcomePanel parent={self} active={activeTab === tabNames[0]} />
+                      <MnemonicPanel parent={self} active={activeTab === tabNames[1]} />
+                      <PasswordPanel parent={self} active={activeTab === tabNames[2]} />
+                      <PinPanel parent={self} active={activeTab === tabNames[3]} />
+                    </section>
 
-<div className="modal active" id="login-dialogue">
-  <div className="content">
-    <div className="row">
-      <div className="ten columns centered text-center">
-        <h2>Login</h2>
-        <p>Enter your passphrase to login or a new one to create a wallet.</p>
-
-        <form>
-          <div className="field">
-            <input className="input" placeholder="Mnemonic"
-               type="text" value={passphrase} onChange={this.handleChange}/>
-          </div>
-        </form>
-        <p className="btn primary medium">
-          <button onClick={this.handleLoginClick} >Login</button>
-        </p>
-        <p className="btn primary medium">
-          <button onClick={this.handleCreateWalletClick} >Create wallet</button>
-        </p>
-        <p/>
-        <p className={warningClasses}>{errorMessage}</p>
-      </div>
-    </div>
-  </div>
-</div>
-      );
-            
+                  </div>
+                </div>
+            );     
         }
+    }
+});
+
+
+
+
+var OLDOLDOLDOLDLogin = React.createClass({
+          setErrorMessage: function (text) {
+              this.setState({errorMessage: text});
+          },
+          getPassPhrase: function () {
+              return this.state.passphrase; 
+          },
+          getInitialState: function () {
+              return {
+                  initializing: false,
+                  passphrase: '',
+                  errorMessage: null
+              };
+          },
+          handleChange: function(event) {
+              this.setState({
+                  initializing: false,
+                  passphrase: event.target.value,
+                  errorMessage: null
+              });
+          },
+          handleLoginClick: function (event) {
+              self = this
+              self.setState({
+                  initializing: true,
+                  passphrase: self.getPassPhrase(),
+                  errorMessage: null
+              });
+              setTimeout(
+                  function () { 
+                      self.props.wallet.initialize(self.getPassPhrase(), 'nothing');
+                      self.setState({
+                          initializing: false,
+                          passphrase: self.getPassPhrase(),
+                          errorMessage: null
+                      });
+                  }, 
+                  100 // allow component to update
+              );
+          },
+          handleCreateWalletClick: function (event) {
+              this.setState({
+                  initializing: false,
+                  passphrase: this.props.wallet.generateMnemonic(),
+                  errorMessage: null
+              });
+          },
+          render: function () {
+              if (this.state.initializing) {
+            return (
+      <div className="modal active" id="login-dialogue">
+        <div className="content">
+          <div className="row">
+            <div className="ten columns centered text-center">
+              <h2>Initializing Wallet ...</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+            );
+              } else if (this.props.wallet.isInitialized()) {
+                  return <div/>;
+              } else {
+                  var passphrase = this.getPassPhrase(),
+                  errorMessage = this.state.errorMessage,
+                  warningClasses = errorMessage ? 'warning alert': '';
+            return (
+
+
+      <div className="modal active" id="login-dialogue">
+        <div className="content">
+          <div className="row">
+            <div className="ten columns centered text-center">
+              <h2>Login</h2>
+              <p>Enter your passphrase to login or a new one to create a wallet.</p>
+
+              <form>
+                <div className="field">
+                  <input className="input" placeholder="Mnemonic"
+                     type="text" value={passphrase} onChange={this.handleChange}/>
+                </div>
+              </form>
+              <p className="btn primary medium">
+                <button onClick={this.handleLoginClick} >Login</button>
+              </p>
+              <p className="btn primary medium">
+                <button onClick={this.handleCreateWalletClick} >Create wallet</button>
+              </p>
+              <p/>
+              <p className={warningClasses}>{errorMessage}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+            );
+
+              }
   }
 });
 
-module.exports = Login;
+module.exports = OLDOLDOLDOLDLogin;
